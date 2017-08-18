21681c3fa662fe5c50059b59716d250f4da9701d
Felquis Gimenes <frgformenton@gmail.com>
2017-08-18 19:11:59 -0300

    chore: add sass-loader to work with DatePicker

--- webapp/config/webpack.config.storybook.js
+++ webapp/config/webpack.config.storybook.js
@@ -41,9 +41,22 @@ module.exports = {
               ],
             },
           },
+          {
+            test: /\.scss$/,
+            use: [
+              {
+                loader: 'style-loader',
+              },
+              {
+                loader: 'css-loader',
+              },
+              {
+                loader: 'sass-loader',
+              },
+            ],
+          },
         ],
       },
     ],
   },
 }
-
