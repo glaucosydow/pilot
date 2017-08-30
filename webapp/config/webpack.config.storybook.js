6500414b4eac5800c4f4cdb7b74e7b8f68a4fb89
Felquis Gimenes <frgformenton@gmail.com>
2017-08-30 13:24:26 -0300

    DatePicker: add proof of concept with react-dates

--- webapp/config/webpack.config.storybook.js
+++ webapp/config/webpack.config.storybook.js
@@ -41,19 +41,19 @@ module.exports = {
               ],
             },
           },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
           {
-            test: /\.scss$/,
-            use: [
-              {
-                loader: 'style-loader',
-              },
-              {
-                loader: 'css-loader',
-              },
-              {
-                loader: 'sass-loader',
-              },
-            ],
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
           },
         ],
       },
